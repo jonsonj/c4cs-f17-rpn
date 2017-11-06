c539d02b8c156f3559edefcd3004171ea72572e8
Jonson Jin <jonsonj@umich.edu>
2017-11-05 19:05:40 -0500

    Set

--- rpn.py
+++ rpn.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 
+import operator
+
+ops = {
+	'+' : operator.add,
+	'-' : operator.sub
+}
+
 def calculate(string):
-    pass
+	stack = list()
+	for token in string.split():
+		try:
+			stack.append(int(token))
+		except ValueError:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			function = ops[token]
+			result = function(arg1, arg2)
+			stack.append(result)
+		print(stack)
+	return stack.pop()
 
 def main():
-    while True:
-        calculate(input("rpn calc> "))
+    	while True:
+        	calculate(input("rpn calc> "))
 
 if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
-    main()
+   	main()
