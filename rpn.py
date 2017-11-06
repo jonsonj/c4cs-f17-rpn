224eef9041399c736a809ae916793e61818eac8a
Jonson Jin <jonsonj@umich.edu>
2017-11-05 19:12:17 -0500

    correct code

--- rpn.py
+++ rpn.py
@@ -2,28 +2,36 @@
 
 import operator
 
-ops = {
-	'+' : operator.add,
-	'-' : operator.sub
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
 }
 
-def calculate(string):
-	stack = list()
-	for token in string.split():
-		try:
-			stack.append(int(token))
-		except ValueError:
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			function = ops[token]
-			result = function(arg1, arg2)
-			stack.append(result)
-		print(stack)
-	return stack.pop()
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
-    	while True:
-        	calculate(input("rpn calc> "))
+    while True:
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
-if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
-   	main()
+if __name__ == '__main__':
+    main()
+
