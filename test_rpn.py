583afc328a2a9a37b01fda6188308d2f70407dd4
Jonson Jin <jonsonj@umich.edu>
2017-11-05 19:14:45 -0500

    test

--- test_rpn.py
+++ test_rpn.py
@@ -8,11 +8,11 @@ class TestBasics(unittest.TestCase):
 		self.assertEqual(2, result)
 	def test_subtract(self):
 	        result = rpn.calculate('5 3 -')
-        	self.assertEqual(2, result)
+		self.assertEqual(2, result)
 	def test_multiply(self):
-        	result = rpn.calculate("5 3 *")
-       		self.assertEqual(15, result)
-    	def test_divide(self):
-        	result = rpn.calculate("6 3 /")
-        	self.assertEqual(2, result)
+		result = rpn.calculate("5 3 *")
+		self.assertEqual(15, result)
+	def test_divide(self):
+		result = rpn.calculate("6 3 /")
+		self.assertEqual(2, result)
 
